1a43d0640627a5fbb67a4c1f8f2a7f65e6904628
Tony Winn <hi@tonywinn.me>
2014-12-03 00:06:52 -0500

    replay_recording can take a recording object
    It seemed weird to have to know that you need to call to_h on a
    recording to get it to replay. This makes the readme example tell a more
    cohesive story.
    
    Also slight cleanup on a redundent or

--- test/integration/replayable_operation_test.rb
+++ test/integration/replayable_operation_test.rb
@@ -1,16 +1,19 @@
 class ReplayableOperationTest < Minitest::Test
   include Examples::InvitationService::TestSetup
 
-  def test_replaying_an_operation
-    recording = perform_for_real
+  def test_replaying_an_operation_from_recording_object
+    recording_object = perform_for_real
+    smtp_service = replay_operation_from recording_object
 
-    smtp_service = build_example_smtp
-
-    second_result = Orchestra.replay_recording(
-      Examples::InvitationService,
-      recording,
-      :smtp => smtp_service,
+    assert_equal(
+      ["captain_sheridan@babylon5.earth.gov"],
+      smtp_service.delivered.keys,
     )
+  end
+
+  def test_replaying_an_operation_from_hash
+    recording_hash = perform_for_real.to_h
+    smtp_service = replay_operation_from recording_hash
 
     assert_equal(
       ["captain_sheridan@babylon5.earth.gov"],
@@ -19,6 +22,17 @@ class ReplayableOperationTest < Minitest::Test
   end
 
   private
+
+  def replay_operation_from(recording)
+    smtp_service = build_example_smtp
+
+    second_result = Orchestra.replay_recording(
+      Examples::InvitationService,
+      recording,
+      :smtp => smtp_service,
+    )
+    smtp_service
+  end
 
   def perform_for_real
     mock_smtp = build_example_smtp
